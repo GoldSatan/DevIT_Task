e02493be15060d97087e2735231cffa803f2cb3f
Vadim Romaniuk <vadim.romanyuk.2003@gmail.com>
2024-02-18 19:22:11 +0200

    create url in backend side

--- Backend/Backend/urls.py
+++ Backend/Backend/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from mysite.views import POSTrequestsView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
+    path('api/requests', POSTrequestsView, name='POSTrequestsView'),
 ]
