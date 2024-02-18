1b93a36a6f3f4d474d08239e336017c9979db760
Vadim Romaniuk <vadim.romanyuk.2003@gmail.com>
2024-02-18 19:22:43 +0200

    create post-view for url in backend side

--- Backend/mysite/views.py
+++ Backend/mysite/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+import time
+import random
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
 
-# Create your views here.
+@csrf_exempt
+def POSTrequestsView(request):
+    if request.method == 'POST':
+        body = json.loads(request.body) 
+        requestsPerSecond = int(body['requestsPerSecond'])
+        
+        response_delay = random.randint(1, 1000) 
+        time.sleep(response_delay / 1000)  
+        
+        response_data = {'index': body['index']}
+        
+        if requestsPerSecond > 50:
+            return JsonResponse({'error': 'Too many requests per second'}, status=429)
+        
+        return JsonResponse(response_data)
+    else:
+        return JsonResponse({'error': 'Only POST requests are allowed'})
